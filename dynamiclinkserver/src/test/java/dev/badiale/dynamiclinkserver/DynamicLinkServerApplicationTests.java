47449550a61eaf136d74f878a53710ba1b5beda3
Lucas Badiale <lcbadial@gmail.com>
2019-02-24 12:05:40 -0300

    Renomeia classe para camel case

--- dynamiclinkserver/src/test/java/dev/badiale/dynamiclinkserver/DynamicLinkServerApplicationTests.java
+++ dynamiclinkserver/src/test/java/dev/badiale/dynamiclinkserver/DynamicLinkServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DynamiclinkserverApplicationTests {
+public class DynamicLinkServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
