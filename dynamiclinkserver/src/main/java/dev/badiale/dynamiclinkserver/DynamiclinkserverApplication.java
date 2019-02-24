3a36107925e4e6c15d3deb9d7d8c77834f5628d8
Lucas Badiale <lcbadial@gmail.com>
2019-02-24 12:04:11 -0300

    Initial commit: Cria projetos vazios

---
+++ dynamiclinkserver/src/main/java/dev/badiale/dynamiclinkserver/DynamiclinkserverApplication.java
@@ -0,0 +1,13 @@
+package dev.badiale.dynamiclinkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamiclinkserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamiclinkserverApplication.class, args);
+	}
+
+}
---
+++ dynamiclinkserver/src/test/java/dev/badiale/dynamiclinkserver/DynamiclinkserverApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.badiale.dynamiclinkserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamiclinkserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
